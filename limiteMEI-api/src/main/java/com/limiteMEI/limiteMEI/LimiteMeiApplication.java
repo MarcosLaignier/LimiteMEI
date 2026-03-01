0732ca11f6b1fdd83b2d38110c0268d7b5c133e7
Marcos Leao <marcos.leao@sonner.com.br>
2026-03-01 16:10:02 -0300

    Adicionar frontend Angular e backend Spring Boot organizados

---
+++ limiteMEI-api/src/main/java/com/limiteMEI/limiteMEI/LimiteMeiApplication.java
@@ -0,0 +1,13 @@
+package com.limiteMEI.limiteMEI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimiteMeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimiteMeiApplication.class, args);
+	}
+
+}
