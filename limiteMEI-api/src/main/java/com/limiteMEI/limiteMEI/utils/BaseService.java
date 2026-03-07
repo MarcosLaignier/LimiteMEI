c042e96644a7f194e7a8af0b9a609f4b02e3a490
Marcos Leao <marcos.leao@sonner.com.br>
2026-03-07 17:55:27 -0300

    LimiteMEI -> Criado Services

--- limiteMEI-api/src/main/java/com/limiteMEI/limiteMEI/utils/BaseService.java
+++ limiteMEI-api/src/main/java/com/limiteMEI/limiteMEI/utils/BaseService.java
@@ -9,105 +9,96 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public abstract class BaseService<T,ID>  {
+public abstract class BaseService<T, ID> {
 
-    @Autowired
-    public GenericUniqueValidator genericUniqueValidator;
+    protected final GenericUniqueValidator genericUniqueValidator;
 
-    /** Metodo padrao de Save
-     * Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo save do repositorio padrao */
-    public T save(T entity) {
-
-        Object var2;
-        try {
-            this.validate(entity);
-            this.beforeSave(entity);
-            entity = (T) this.getRepository().save(entity);
-            this.afterSave(entity);
-            var2 = entity;
-        } finally {
-        }
-
-        return (T) var2;
+    protected BaseService(GenericUniqueValidator genericUniqueValidator) {
+        this.genericUniqueValidator = genericUniqueValidator;
     }
 
+    protected abstract JpaRepository<T, ID> getRepository();
 
+    /** Metodo padrao de Save
+     * Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo save do repositorio padrao
+     */
+    public T save(T entity) {
+
+        validate(entity);
+        beforeSave(entity);
+
+        entity = getRepository().save(entity);
+
+        afterSave(entity);
+
+        return entity;
+    }
 
     /** Metodo padrao de Update
-     * Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo update do repositorio padrao */
-    public T update(T entity) throws ServiceException {
+     * Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo update do repositorio padrao
+     */
+    public T update(T entity) {
 
-        Object var2;
-        try {
-            this.validate(entity);
-            this.beforeUpdate(entity);
-            entity = (T) this.getRepository().save(entity);
-            this.afterUpdate(entity);
-            var2 = entity;
-        } finally {
-        }
+        validate(entity);
+        beforeUpdate(entity);
 
-        return (T) var2;
+        entity = getRepository().save(entity);
+
+        afterUpdate(entity);
+
+        return entity;
     }
 
     /** Metodo padrao de Delete
-     * Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo delete do repositorio padrao */
-    public T delete(T entity) throws ServiceException {
+     * Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo delete do repositorio padrao
+     */
+    public void delete(T entity) {
 
-        Object var2;
-        try {
-            this.validateDelete(entity);
-            this.beforeDelete(entity);
-            this.getRepository().delete(entity);
-            this.afterDelete(entity);
-            var2 = entity;
-        } finally {
+        validateDelete(entity);
+        beforeDelete(entity);
 
-        }
+        getRepository().delete(entity);
 
-        return (T) var2;
+        afterDelete(entity);
     }
 
     /**
      * Metodo padrao de buscar todos os dados da entity
      */
     public List<T> findAll() {
-        return this.getRepository().findAll();
+        return getRepository().findAll();
     }
 
     /** Metodo padrao de buscar por ID da entity */
 
     public T getById(ID id) {
-        return (T) this.getRepository().findById(id).orElseThrow();
+        return getRepository()
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Registro não encontrado"));
     }
 
-
-    protected void validate(T entity) throws ServiceException {
-        ValidateMetodsUtils.validate(entity,  genericUniqueValidator);
+    protected void validate(T entity) {
+        ValidateMetodsUtils.validate(entity, genericUniqueValidator);
     }
 
-    protected void validateDelete(T entity) throws ServiceException {
+    protected void validateDelete(T entity) {
     }
 
-    protected void beforeSave(T entity) throws ServiceException {
+    protected void beforeSave(T entity) {
     }
 
-    protected void afterSave(T entity) throws ServiceException {
+    protected void afterSave(T entity) {
     }
 
-    protected void beforeUpdate(T entity) throws ServiceException {
+    protected void beforeUpdate(T entity) {
     }
 
-    protected void afterUpdate(T entity) throws ServiceException {
+    protected void afterUpdate(T entity) {
     }
 
-    protected void beforeDelete(T entity) throws ServiceException {
+    protected void beforeDelete(T entity) {
     }
 
-    protected void afterDelete(T entity) throws ServiceException {
+    protected void afterDelete(T entity) {
     }
-
-    public abstract JpaRepository getRepository();
-
 }
-
